c274ae5d49885b1ee786478c798d9e72a744c559
Chris Manson <chris@stonecircle.io>
2017-12-06 23:21:14 +0000

    bumping authmaker-verify and fixing tests

--- test/fixtures/users.js
+++ test/fixtures/users.js
@@ -1,75 +1,62 @@
-var authmakerVerify = require('authmaker-verify');
-var moment = require('moment');
-var mongoose = require('mongoose');
-var Q = require('q');
+const authmakerVerify = require('authmaker-verify');
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Q = require('q');
 
-var getModel = authmakerVerify.getModel;
-
-// var usersToCreate = [{
-//     _id: mongoose.Types.ObjectId(),
-//     username: 'testuser1@bloo.ie',
-//     clientId: 'testChatsFixture',
-//     date: moment().subtract(1, 'hours').toDate()
-// }];
-
-var sessionsToCreate = [{
-    access_token: "valid_access_token_1",
-    expiryDate: moment().add(1, 'hours').toDate(),
-    userId: mongoose.Types.ObjectId()
+const sessionsToCreate = [{
+  accessToken: 'valid_access_token_1',
+  expiry: moment().add(1, 'hours').toDate(),
+  userId: mongoose.Types.ObjectId(),
 }, {
-    access_token: "valid_face_permission",
-    expiryDate: moment().add(1, 'hours').toDate(),
-    scopes: ['face_permissions'],
-    userId: mongoose.Types.ObjectId()
+  accessToken: 'valid_face_permission',
+  expiry: moment().add(1, 'hours').toDate(),
+  scopes: ['face_permissions'],
+  userId: mongoose.Types.ObjectId(),
 }, {
-    access_token: 'expired_access_token',
-    expiryDate: moment().subtract(1, 'seconds').toDate(),
-    userId: mongoose.Types.ObjectId()
+  accessToken: 'expired_access_token',
+  expiry: moment().subtract(1, 'seconds').toDate(),
+  userId: mongoose.Types.ObjectId(),
 }, {
-    access_token: 'valid_rate_limit_5_days',
-    expiryDate: moment().add(1, 'hours').toDate(),
-    scopes: ['face_limit_5_days'],
-    userId: mongoose.Types.ObjectId()
+  accessToken: 'valid_rate_limit_5_days',
+  expiry: moment().add(1, 'hours').toDate(),
+  scopes: ['face_limit_5_days'],
+  userId: mongoose.Types.ObjectId(),
 }, {
-    access_token: 'valid_rate_limit_5_days_user2',
-    expiryDate: moment().add(1, 'hours').toDate(),
-    scopes: ['face_limit_5_days'],
-    userId: mongoose.Types.ObjectId()
+  accessToken: 'valid_rate_limit_5_days_user2',
+  expiry: moment().add(1, 'hours').toDate(),
+  scopes: ['face_limit_5_days'],
+  userId: mongoose.Types.ObjectId(),
 }, {
-    access_token: 'valid_rate_limit_5_second',
-    expiryDate: moment().add(1, 'hours').toDate(),
-    scopes: ['face_limit_5_seconds'],
-    userId: mongoose.Types.ObjectId()
+  accessToken: 'valid_rate_limit_5_second',
+  expiry: moment().add(1, 'hours').toDate(),
+  scopes: ['face_limit_5_seconds'],
+  userId: mongoose.Types.ObjectId(),
 }];
 
 function init() {
-    return getModel('oauthSession').then(function(oauthSession){
-        return oauthSession.create(sessionsToCreate);
-    });
+  return authmakerVerify.models.oauthSession.create(sessionsToCreate);
 }
 
 function reset() {
-    //only allow this in test
-    if (process.env.NODE_ENV === 'test') {
-        return authmakerVerify.getConnection().then(function(connection){
-            var collections = connection.collections;
+  // only allow this in test
+  if (process.env.NODE_ENV === 'test') {
+    return authmakerVerify.getConnection().then(function (connection) {
+      const collections = connection.collections;
 
-            var promises = Object.keys(collections).map(function(collection) {
-                return Q.ninvoke(collections[collection], 'remove');
-            });
+      const promises = Object.keys(collections).map(function (collection) {
+        return Q.ninvoke(collections[collection], 'remove');
+      });
 
-            return Q.all(promises);
-        });
-    } else {
-        var errorMessage = 'Excuse me kind sir, but may I enquire as to why you are currently running reset() in a non test environment? I do propose that it is a beastly thing to do and kindly ask you to refrain from this course of action. Sincerely yours, The Computer.';
-        console.log(errorMessage);
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-    }
+      return Q.all(promises);
+    });
+  }
+  const errorMessage = 'Excuse me kind sir, but may I enquire as to why you are currently running reset() in a non test environment? I do propose that it is a beastly thing to do and kindly ask you to refrain from this course of action. Sincerely yours, The Computer.';
+  console.log(errorMessage);
+  console.error(errorMessage);
+  throw new Error(errorMessage);
 }
 
 module.exports = {
-    init: init,
-    reset: reset,
-    getModel: getModel
+  init,
+  reset,
 };
--- test/index.js
+++ test/index.js
@@ -1,29 +1,29 @@
-var http = require('http');
-var nconf = require('nconf');
+const http = require('http');
+const nconf = require('nconf');
 
-var express = require('express');
+const express = require('express');
 
-var authmakerVerify = require('authmaker-verify');
+const authmakerVerify = require('authmaker-verify');
 
-var httpServer;
+let httpServer;
 
 nconf.defaults({
-    "mongo": {
-        "authmaker": {
-            "db": "authmaker-verify-express-test",
-            "host": "localhost",
-            "port": 27017
-        }
-    }
+  authmaker: {
+    mongo: {
+      db: 'authmaker-verify-express-test',
+      host: 'localhost',
+      port: 27017,
+    },
+  },
 });
 
-before(function() {
-    global.app = express();
-    //create http server
-    httpServer = http.createServer(global.app).listen(56773);
-    authmakerVerify.connectMongo(nconf);
+before(() => {
+  global.app = express();
+  // create http server
+  httpServer = http.createServer(global.app).listen(56773);
+  authmakerVerify.init(nconf);
 });
 
-after(function(done) {
-    httpServer.close(done);
+after((done) => {
+  httpServer.close(done);
 });
